ecf298178c9ca1e23683a89c12aedb537c779bee
caiohmg <108705024+caiohmg@users.noreply.github.com>
2023-12-27 19:43:37 -0300

    .

--- js/meu-arquivo.js
+++ js/meu-arquivo.js
@@ -1,8 +1,10 @@
 function imgSlider(e) {
+  console.log('Changing image to:', e);
   document.querySelector('#phone').src = e;
 }
 
 function circleChange(color) {
+  console.log('Changing circle color to:', color);
   const circle = document.querySelector('.circle');
   circle.style.background = color;
 }
